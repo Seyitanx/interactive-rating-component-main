fb94258b96d92736881951eb35d6e7cc742f5bed
Seyitanx <seyitanadepegba@gmail.com>
2022-05-22 19:10:28 +0100

    .

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
-let numberPicked=
+let numberPicked;
 
-function () {
-    buttonOne= document.getElementById("#first-button");
-    buttonTwo = document.getElementById("#second-button");
-    buttonThree = document.getElementById("#third-button");
-    buttonFour = document.getElementById("#fourth-button");
-    buttonFive = document.getElementById("#fifth-button");
-    
-    
+buttonOne = document.getElementById("first-button");
+
+buttonTwo = document.getElementById("second-button");
+
+buttonThree = document.getElementById("third-button");
+buttonFour = document.getElementById("fourth-button");
+buttonFive = document.getElementById("fifth-button");
+
+buttonOne.addEventListener("click", function () {
+  numberPicked = buttonOne.innerHTML;
+});
+
+buttonTwo.addEventListener("click", function () {
+  numberPicked = buttonTwo.innerHTML;
+  console.log(numberPicked);
+});
+buttonThree.addEventListener("click", function () {
+  numberPicked = buttonThree.innerHTML;
+});
+buttonFour.addEventListener("click", function () {
+  numberPicked = buttonFour.innerHTML;
+});
+buttonFive.addEventListener("click", function () {
+  numberPicked = buttonFive.innerHTML;
+});
+
+//this is meant to be for the toggle
+function submit() {
+  let button = document.getElementById("thankyou-card");
+  let card= document.getElementById("rating-card")
+  card.style.display === "none";
+  button.style.display === "block";
+  console.log(submit)
+  
 }
-buttonOne.addEventListener("click", numberPicked);
+    let submitButton = document.querySelector("#btn");
+
+submitButton.addEventListener("click", submit);
